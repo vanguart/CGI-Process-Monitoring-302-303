84bc7214a9b77be4c76797f276b0753097240be2
josesobral22005813 <a22005813@alunos.ulht.pt>
2023-03-27 15:01:15 +0100

    Removed templates from wrong app

--- apps/operational_platform/urls.py
+++ apps/operational_platform/urls.py
@@ -2,8 +2,4 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
-urlpatterns = [
-    path('businessExceptions', views.businessExceptions_page_view, name='businessExceptions'),
-    path('correcaoDocumentos', views.correcaoDocumentos_page_view, name='correcaoDocumentos'),
-    path('reportarErrosNoSistema', views.reportarErrosNoSistema_page_view, name='reportarErrosNoSistema')
-]
+urlpatterns = []
