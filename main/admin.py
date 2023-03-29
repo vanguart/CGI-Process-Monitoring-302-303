53538b3e7638b3e3d7aa1b7905d633d02599a608
Joao-Eleuterio <a22007237@alunos.ulht.pt>
2023-03-29 18:17:15 +0100

    -Update admin

--- main/admin.py
+++ main/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Permition
-from .models import Perfil
+#from .models import Permition
+#from .models import Perfil
 from .models import Team
 from .models import TaskType
 from .models import SLA
