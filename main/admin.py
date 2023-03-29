09960f140f49fb1cc8abeea7dcf551e5008f641a
Alexandre-Costa-22007578 <a22007578@alunos.ulht.pt>
2023-03-29 17:15:57 +0100

    a

--- main/admin.py
+++ main/admin.py
@@ -13,6 +13,8 @@ from .models import Process
 from .models import Task
 from .models import Logs
 from .models import Label
+from .models import ProcessConfiguration
+from .models import Task_Configuration
 
 class PerfilHorizontal(admin.ModelAdmin):
     filter_horizontal = ("permitionPerfil",)
@@ -20,16 +22,21 @@ class PerfilHorizontal(admin.ModelAdmin):
 
 class TeamHorizontal(admin.ModelAdmin):
     filter_horizontal = ("team", "tasks")
+    
+class ProcessesHorizontal(admin.ModelAdmin):
+    filter_horizontal = ("label")
 
 admin.site.register(Permition)
+admin.site.register(Label)
 admin.site.register(Perfil, PerfilHorizontal)
 admin.site.register(TaskType)
 admin.site.register(SLA)
 admin.site.register(LogsType)
 admin.site.register(ProcessType)
 admin.site.register(User)
-admin.site.register(Process)
+admin.site.register(Process, ProcessesHorizontal)
 admin.site.register(Task)
 admin.site.register(Team, TeamHorizontal)
 admin.site.register(Logs)
-admin.site.register(Label)
+admin.site.register(ProcessConfiguration)
+admin.site.register(Task_Configuration) 
