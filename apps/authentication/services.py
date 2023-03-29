55349ef1ae6863b4f7e8a7889b8bd003357d6965
Alexandre-Costa-22007578 <a22007578@alunos.ulht.pt>
2023-03-29 16:24:10 +0100

    settings.py changes to send emails servicer.py authentication function to genera code and send email services.py overall_statistics functions with overall stats

---
+++ apps/authentication/services.py
@@ -0,0 +1,26 @@
+import random
+import string
+
+# function that generates an E-mail code
+def generatesEmailCode(userEmailInput):
+    userEmail = User.objects.filter(email=userEmailInput)
+
+    if len(userEmail) == 0:
+        print("there is nothing in the database")
+      
+    length = 10
+    # With combination of lower and upper case
+    result_str = ''.join(random.choice(string.ascii_letters) for i in range(length))
+
+    return result_str
+
+#function that sends an email with a code in order to reset password
+def sendEmailWithGeneratedCode(userEmailInput):
+    code = generatesEmailCode(userEmailInput)
+
+    send_mail(
+        'Reset Password',
+        'Here is the code u need to reset your password\n {code}',
+        'cgiprocessmonitor@gmail.com',
+        ['88alexcosta88@gmail.com'],
+    )
