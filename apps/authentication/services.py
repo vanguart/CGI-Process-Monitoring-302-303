f28d3a28c41b0ae8a9cea57df9b54a1a8d03dac4
Joao-Eleuterio <a22007237@alunos.ulht.pt>
2023-03-30 09:25:43 +0100

    - User name conflict error in User table

--- apps/authentication/services.py
+++ apps/authentication/services.py
@@ -4,6 +4,7 @@ from main.models import User
 from django.core.mail import send_mail
 from django.contrib.auth.models import User
 
+
 # function that generates an E-mail code
 def generatesEmailCode(userEmailInput):
     userEmail = User.objects.filter(email=userEmailInput)
@@ -11,14 +12,15 @@ def generatesEmailCode(userEmailInput):
     if len(userEmail) == 0:
         print("there is nothing in the database")
         return None
-      
+
     length = 10
     # With combination of lower and upper case
     result_str = ''.join(random.choice(string.ascii_letters) for i in range(length))
 
     return result_str
 
-#function that sends an email with a code in order to reset password
+
+# function that sends an email with a code in order to reset password
 def sendEmailWithGeneratedCode(userEmailInput):
     code = generatesEmailCode(userEmailInput)
 
@@ -31,6 +33,10 @@ def sendEmailWithGeneratedCode(userEmailInput):
 
 
 def verifyEmailOnDataBase(userEmailInput):
-    userEmail = User.objects.filter(EMAIL_FIELD=userEmailInput).exists()
+    userEmail = User.objects.filter(email=userEmailInput).exists()
     return userEmail
-    
+
+# def change_password(id_user, newpassword):
+# preciso receber o id/email do utilizador
+# procurar o utilizador  user = User.objects.fileter(id=id_user)
+# alterar palavra-passe
