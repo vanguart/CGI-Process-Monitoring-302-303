69dd16e358e0964a66d63426e90feaafaf540195
ricardocleto22006526 <a22006526@alunos.ulht.pt>
2023-03-31 20:28:14 +0100

    Updated `Views.py` of process_listing

--- apps/process_listing/views.py
+++ apps/process_listing/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-
+from main.models import Process
 # Create your views here.
 
 def process_listing_page_view(request):
-    return render(request, 'process_listing/process_listing.html')
+    context = {'process': Process.objects.all()}
+    
+    return render(request, 'process_listing/process_listing.html', context)
