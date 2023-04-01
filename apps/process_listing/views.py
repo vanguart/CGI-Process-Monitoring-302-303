4cc196499ad09802ded0e79371855df60a827b99
ricardocleto22006526 <a22006526@alunos.ulht.pt>
2023-04-01 15:14:44 +0100

    Added Filter option on `Process_listing`

--- apps/process_listing/views.py
+++ apps/process_listing/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
-from main.models import Process
+from main.models import Process, Label
 # Create your views here.
 
 def process_listing_page_view(request):
-    context = {'process': Process.objects.all()}
+
+    if request.GET.get('filtros') == "noFilter" :
+        query = Process.objects.all()
+
+    if request.GET.get('filtros') != "noFilter":
+        featured_filter = request.GET.get('filtros')
+        query = Process.objects.filter(label__name=featured_filter)
+
+
+    context = {
+        'process': query,
+        'filtros': Label.objects.all(),
+    }
     
     return render(request, 'process_listing/process_listing.html', context)
