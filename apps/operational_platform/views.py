78ed1f74980175f5c3f276dc3acb19a57a356c75
ricardocleto22006526 <a22006526@alunos.ulht.pt>
2023-04-01 19:30:29 +0100

    Updated `BusinessException` Views/HTML/CSS

--- apps/operational_platform/views.py
+++ apps/operational_platform/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
+from main.models import Task, Task_Configuration, TaskType
 
 # Create your views here.
 
 def businessExceptions_page_view(request):
-	return render(request, 'operational_platform/businessExceptions.html')
+
+
+	context = {
+		'tasks': Task.objects.all(),
+		'tasks_config':Task_Configuration.objects.all(),
+		'tasks_type': TaskType.objects.all()
+	}
+
+	return render(request, 'operational_platform/businessExceptions.html', context)
 
 def correcaoDocumentos_page_view(request):
 	return render(request, 'operational_platform/correcaoDocumentos.html')
