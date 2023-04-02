cc18d7feadefd0c540e106657d86c32e221a8de9
ricardocleto22006526 <a22006526@alunos.ulht.pt>
2023-04-02 19:12:51 +0100

    Done `Team task list & TeamName of user logged` on Views (Operational_platform)

--- apps/operational_platform/views.py
+++ apps/operational_platform/views.py
@@ -1,15 +1,31 @@
 from django.shortcuts import render
-from main.models import Task, TaskConfiguration, TaskType
+from main.models import Task, TaskConfiguration, TaskType, Team, UserProfile
 
-# Create your views here.
+# DEBUGGING
+from apps.logs.services import getTaskLogs
+
 
 def businessExceptions_page_view(request):
+	tasks_list = []
+	# User logado
+	username = request.user.username
 
+	#Nome da equipa do User logado
+	teamOfUser = Team.objects.filter(members__user__username=username).first()
+	
+	#Tarefas da equipa do User logado
+	if (teamOfUser != None) and (teamOfUser.tasks.exists()):
+		
+		teamTasks = teamOfUser.tasks.all()
 
+		for task in teamTasks:
+			tasks_list.append(task)
+
+	#a = getTaskLogs(1)
 	context = {
-		'tasks': Task.objects.all(),
-		'tasks_config':TaskConfiguration.objects.all(),
-		'tasks_type': TaskType.objects.all()
+		'teamName': teamOfUser,
+		'teamtasks': tasks_list,
+		#'a': a,
 	}
 
 	return render(request, 'operational_platform/businessExceptions.html', context)
