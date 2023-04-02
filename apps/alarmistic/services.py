6a31ccdfa9795b7ff8a6713bd996c18044d4aede
JoaoEleuterio <a22007237@alunos.ulht.pt>
2023-04-02 22:36:01 +0100

    - email sending transfer to alarmistica

---
+++ apps/alarmistic/services.py
@@ -0,0 +1,88 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+from main.models import Log, Task, UserProfile
+import schedule
+import time
+import datetime
+
+
+def enviamail(email, subject, body):
+    # informações da conta
+    email_usuario = 'a22007237@alunos.ulht.pt'
+    senha = 'JPcse1992'
+
+    # informações do destinatário
+    para = email
+    # criando mensagem
+    msg = MIMEMultipart()
+    msg['From'] = email_usuario
+    msg['To'] = para
+    msg['Subject'] = subject
+    msg.attach(MIMEText(body, 'plain'))
+
+    # conectando ao servidor SMTP
+    server = smtplib.SMTP('smtp-mail.outlook.com', 587)
+    server.starttls()
+
+    # fazendo login na conta
+    server.login(email_usuario, senha)
+
+    # enviando o e-mail
+    texto = msg.as_string()
+    server.sendmail(email_usuario, para, texto)
+
+    # encerrando a conexão
+    server.quit()
+
+
+# Fazer timer de enviar email diariamente
+
+def enviarEmailErro():
+    subject = "Task with "
+
+    for logs in Log.objects.all():
+
+        if logs.logType.name == "Warning":
+            subject += "Warning"
+
+        if logs.logType.name == "Fatal Error":
+            subject += "Fatal Error"
+
+        for task in Task.objects.all():
+
+            if task == logs.task:
+                body = "You have " + subject.lower()
+                email = task.user.user.email
+                enviamail(email, subject, body)
+
+
+def enviarEmailObjetivo(threshold):
+    subject = "Goal Low"
+    for users in UserProfile.all():
+        email = users.user.email
+        if users.goal < threshold:
+            if users.idPerfil == 'RPA':
+                body = "The RPA " + users.user.name + " goal is low!"
+                enviamail(email, subject, body)
+            else:
+                body = "Your goal is low!"
+                enviamail(email, subject, body)
+
+
+def enviarEmailTarefasRealizarToday():
+    subject = "Tasks to-do Today"
+    body = "To-do today:\n"
+
+    # enviamail(email,subject,body)
+
+
+# todos os dias envia os emails
+schedule.every(24).hours.do(enviarEmailErro)
+schedule.every(24).hours.do(enviarEmailTarefasRealizarToday)
+schedule.every(24).hours.do(enviarEmailObjetivo(70))
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
--- apps/logs/services.py
+++ apps/logs/services.py
@@ -1,10 +1,6 @@
 # Connection API Orchestrator
 import io
 import json
-import smtplib
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-
 import requests
 import msoffcrypto
 import pandas as pd
@@ -124,92 +120,6 @@ def criarItemQueue():
 def removerItemQueue():
     print('hi')
 
-
-def enviamail(email, subject, body):
-    # informações da conta
-    email_usuario = 'a22007237@alunos.ulht.pt'
-    senha = 'JPcse1992'
-
-    # informações do destinatário
-    para = email
-    # criando mensagem
-    msg = MIMEMultipart()
-    msg['From'] = email_usuario
-    msg['To'] = para
-    msg['Subject'] = subject
-    msg.attach(MIMEText(body, 'plain'))
-
-    # conectando ao servidor SMTP
-    server = smtplib.SMTP('smtp-mail.outlook.com', 587)
-    server.starttls()
-
-    # fazendo login na conta
-    server.login(email_usuario, senha)
-
-    # enviando o e-mail
-    texto = msg.as_string()
-    server.sendmail(email_usuario, para, texto)
-
-    # encerrando a conexão
-    server.quit()
-
-
-# Fazer timer de enviar email diariamente
-
-def enviarEmailErro():
-    
-    subject = "Task with "
-
-    for logs in Log.objects.all():
-
-        if logs.logType.name == "Warning":
-            subject += "Warning"
-
-        if logs.logType.name== "Fatal Error":
-            subject += "Fatal Error"
-
-        for task in Task.objects.all():
-
-            if task == logs.task:
-                body = "You have " + subject.lower()
-                email = task.user.user.email
-                enviamail(email, subject, body)
-
-
-def enviarEmailObjetivo(threshold):
-    subject = "Goal Low"
-    for users in UserProfile.all():
-        email = users.user.email
-        if users.goal < threshold:
-            if users.idPerfil == 'RPA':
-                body = "The RPA " + users.user.name + " goal is low!"
-                enviamail(email, subject, body)
-            else:
-                body = "Your goal is low!"
-                enviamail(email, subject, body)
-
-
-def enviarEmailTarefasRealizarToday():
-    subject = "Tasks to-do Today"
-    body = "To-do today:\n"
-
-    # enviamail(email,subject,body)
-
-
-
-"""
-
-def getTaskLogs(taskRef):
-    log = Log.objects.filter(task__id=taskRef).all()
-    textLog = ""
-    for logging in log:
-        with logging.ficheiro.open('r') as file:
-            conteudo = file.read()
-            textLog += conteudo + '\n'
-            print(conteudo)
-    return textLog
-
-"""
 
 def getTaskLogs(taskRef):
     log = Log.objects.filter(task__id=taskRef).all()
