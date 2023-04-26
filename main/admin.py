0d347b9e43aeafdbcd78808c69713484e57a3f16
ricardocleto22006526 <a22006526@alunos.ulht.pt>
2023-04-26 13:33:39 +0100

    Updated `Data Base`

--- main/admin.py
+++ main/admin.py
@@ -1,14 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Team
+from .models import Team, Skill
 from .models import TaskType
 from .models import SLA
 from .models import LogType
 from .models import ProcessType
 from .models import UserProfile
-from .models import Process
-from .models import Task
+from .models import QueueProcess
+from .models import QueueTask
 from .models import Log
 from .models import Label
 from .models import ProcessConfiguration
@@ -17,23 +17,32 @@ from .models import Reporting
 
 
 class TeamHorizontal(admin.ModelAdmin):
-    filter_horizontal = ("permissions",)
+    filter_horizontal = ("idPermissions", "idSkils",)
 
 
-class ProcessesHorizontal(admin.ModelAdmin):
-    filter_horizontal = ("labels",)
+class QueueProcessesHorizontal(admin.ModelAdmin):
+    filter_horizontal = ("idLabels",)
+
+
+class ProcessesConfigurationHorizontal(admin.ModelAdmin):
+    filter_horizontal = ("idSkills",)
+
+
+class UserProfileHorizontal(admin.ModelAdmin):
+    filter_horizontal = ("idSkills",)
 
 
 admin.site.register(Label)
+admin.site.register(Skill)
 admin.site.register(TaskType)
 admin.site.register(SLA)
 admin.site.register(LogType)
 admin.site.register(ProcessType)
-admin.site.register(UserProfile)
-admin.site.register(Process, ProcessesHorizontal)
-admin.site.register(Task)
+admin.site.register(UserProfile, UserProfileHorizontal)
+admin.site.register(QueueProcess, QueueProcessesHorizontal)
+admin.site.register(QueueTask)
 admin.site.register(Team, TeamHorizontal)
 admin.site.register(Log)
-admin.site.register(ProcessConfiguration)
+admin.site.register(ProcessConfiguration, ProcessesConfigurationHorizontal)
 admin.site.register(TaskConfiguration)
 admin.site.register(Reporting)
