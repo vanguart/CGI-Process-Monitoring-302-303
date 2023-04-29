5a949dd50cb6a348ab87df0874e1bcb15d90906f
Alexandre-Costa-22007578 <a22007578@alunos.ulht.pt>
2023-04-29 18:20:54 +0100

    cleanOutputData function

--- apps/operational_platform/services.py
+++ apps/operational_platform/services.py
@@ -12,7 +12,7 @@ def removeProcess(process_id):
     process.update(idUser=None)
     
     
-def getInputData(queue_task_id,dados = False):
+def getInputData(queue_task_id):
     task = QueueTask.objects.get(id=queue_task_id)
     data = str(task.inputData)
     fields =  []
@@ -24,3 +24,20 @@ def getInputData(queue_task_id,dados = False):
         dataAfterProcessing.append(individualData[1])
     result = list(zip(fields, dataAfterProcessing))
     return result
+
+def cleanOutputData(task_id):
+    task = QueueTask.objects.get(id=task_id)
+    data = str(task.outputData)
+
+    data = data.replace("{","").replace("}","").replace("\'","")
+    groupData = data.split(",")
+    result = ""
+    for j in range(0,len(groupData)):
+        individualData = groupData[j].split(":")
+        if j == 0:
+            result = result + individualData[0].strip() +":" + individualData[1].strip()
+        else:
+            result = result + ";"  + individualData[0].strip() +":" + individualData[1].strip()
+
+    task.outputData = result
+    task.save()
