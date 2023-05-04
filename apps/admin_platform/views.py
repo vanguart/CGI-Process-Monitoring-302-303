171b2b101c7e2cabdd117b08cd7bbbdaab87c5d1
ricardocleto22006526 <a22006526@alunos.ulht.pt>
2023-05-04 17:36:09 +0100

    Corrected `commentary on Admin Views`

--- apps/admin_platform/views.py
+++ apps/admin_platform/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
+from apps.admin_platform.services import *
 from main.models import *
 
 
 # Create your views here.
 
 def adminPage_view(request):
+
     allUsersDataBase = UserProfile.objects.all()
+
     tasksDone = {}
     tasksTodo = {}
 
@@ -22,12 +25,12 @@ def adminPage_view(request):
 
             for task in QueueTask.objects.filter(idProcess=processo.id):
 
-                if task.state == "Completed":  # tarefas a realizar
+                if task.state == "Completed": # tarefas realizadas 
 
                     tasksDoneCount += 1
                     tasksDone.update({user.id: tasksDoneCount})
 
-                elif task.state == "Stopped":  # tarefas realizadas
+                elif task.state == "Stopped": # tarefas a realizar 
 
                     tasksTodoCount += 1
                     tasksTodo.update({user.id: tasksTodoCount})
@@ -43,3 +46,23 @@ def adminPage_view(request):
     }
 
     return render(request, 'admin_platform/adminPage.html', context)
+
+
+
+def adminGerirCargos_view(request):
+
+    allUsersDataBase = UserProfile.objects.all()
+    allTeamsDataBase = Team.objects.all()
+
+    if request.method == 'POST':
+        team_id, user_id = request.POST.get('equipa').split('-')
+        changeTeam(team_id,user_id)
+
+
+
+    context = {
+        'allUsersNames': allUsersDataBase,
+        'allTeamNames': allTeamsDataBase,
+    }
+
+    return render(request, 'admin_platform/gerirCargos.html', context)
