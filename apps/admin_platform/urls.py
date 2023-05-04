a03d2db2e8d5b5cf2e34f7e2000ecc0d3725840a
ricardocleto22006526 <a22006526@alunos.ulht.pt>
2023-05-04 14:35:59 +0100

    Changed `ToDoAmin to adminPage`

--- apps/admin_platform/urls.py
+++ apps/admin_platform/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('todoAdmin/', views.todoAdmin_page_view, name='todoAdmin'),
+    path('adminPage', views.adminPage_view, name='adminPage'),
 ]
