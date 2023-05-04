298bacefe30ad17056d6cbc2f74e5cf8c55e10d0
ricardocleto22006526 <a22006526@alunos.ulht.pt>
2023-05-04 15:53:58 +0100

    Cleared counters on `Admin_Platform (Views)`

--- apps/admin_platform/views.py
+++ apps/admin_platform/views.py
@@ -12,17 +12,29 @@ def adminPage_view(request):
     tasksDoneCount = 0
     tasksTodoCount = 0
 
+
     for user in allUsersDataBase:
+
         tasksDone.update({user.id: tasksDoneCount})
         tasksTodo.update({user.id: tasksTodoCount})
+
         for processo in QueueProcess.objects.filter(idUser=user.id):
+
             for task in QueueTask.objects.filter(idProcess=processo.id):
+
                 if task.state == "Completed":  # tarefas a realizar
+
                     tasksDoneCount += 1
                     tasksDone.update({user.id: tasksDoneCount})
+
                 elif task.state == "Stopped":  # tarefas realizadas
+
                     tasksTodoCount += 1
                     tasksTodo.update({user.id: tasksTodoCount})
+
+        tasksDoneCount = 0
+        tasksTodoCount = 0
+
 
     context = {
         'allUsersNames': allUsersDataBase,
