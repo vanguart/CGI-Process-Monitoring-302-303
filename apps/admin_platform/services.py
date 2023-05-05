c84b7b7f2f3de33fd628e17e365744d8ec6270d7
JoaoEleuterio <a22007237@alunos.ulht.pt>
2023-05-05 10:15:58 +0100

    - update `views.py` changeTeam

--- apps/admin_platform/services.py
+++ apps/admin_platform/services.py
@@ -10,10 +10,18 @@ def changeTeam(team_id, user_id):
         user_profile.update(idTeam=team_id)
 """
 
+"""
 # TODO JOAO & RICARDO (CORRIGIR ESTA PARTE PARA O TEAMLIDER - TEAMLIDER NAO ALTERA DE TEAM)
 def changeTeam(team_id, user_id):
     user_profile = UserProfile.objects.filter(id=user_id)
     team_lider = Team.objects.get(id=team_id)
-    print(f"{team_lider.idTeamLider}-------------{user_profile.first().id}" )
+    print(f"{team_lider.idTeamLider}-------------{user_profile.first().id}")
     if team_lider.idTeamLider == user_profile.first():
+        user_profile.update(idTeam=team_id)"""
+
+
+def changeTeam(team_id, user_id):
+    user_profile = UserProfile.objects.filter(id=user_id)
+    lider_de_outra_equipe = Team.objects.filter(idTeamLider=user_profile.first().id).exclude(id=team_id).exists()
+    if not lider_de_outra_equipe:
         user_profile.update(idTeam=team_id)
