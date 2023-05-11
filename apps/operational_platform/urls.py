2a14b5ccb86171642557c263cff510ab8ff4f0c1
JoaoEleuterio <a22007237@alunos.ulht.pt>
2023-05-11 22:42:11 +0100

    - update  `urls.py`

--- apps/operational_platform/urls.py
+++ apps/operational_platform/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('businessExceptions', views.businessExceptions_page_view, name='businessExceptions'),
-    path('changeSkills/<int:user_id>', views.changeSkills_page_view, name='changeSkills'),
-    path('correcaoDocumentos/<int:task_id>', views.correcaoDocumentos_page_view, name='correcaoDocumentos'),
+    path('changeSkills/<int:userId>', views.changeSkills_page_view, name='changeSkills'),
+    path('correcaoDocumentos/<int:taskId>', views.correcaoDocumentos_page_view, name='correcaoDocumentos'),
     path('reportarErrosNoSistema', views.reportarErrosNoSistema_page_view, name='reportarErrosNoSistema')
 ]
