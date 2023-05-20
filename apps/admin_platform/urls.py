80df8ae5fadaf2c0c3c4a167aba399b67adc453f
ricardocleto22006526 <a22006526@alunos.ulht.pt>
2023-05-20 16:43:06 +0100

    Added `gerirEquipaProcesso` page on Admin(Urls)

--- apps/admin_platform/urls.py
+++ apps/admin_platform/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('adminPage', views.adminPage_view, name='adminPage'),
     path('gerirCargosUsers', views.adminGerirCargosUsers_view, name='gerirCargosUsers'),
-    path('gerirCargosTeams', views.adminGerirCargosTeams_view, name='gerirCargosTeams')
+    path('gerirCargosTeams', views.adminGerirCargosTeams_view, name='gerirCargosTeams'),
+    path('gerirEquipaProcesso', views.adminGerirEquipaProcesso_view, name='gerirEquipaProcesso')
 ]
