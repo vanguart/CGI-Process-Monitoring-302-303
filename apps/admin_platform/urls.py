61edf12da0da86c66cd3ca46c769cc68f3d7f319
ricardocleto22006526 <a22006526@alunos.ulht.pt>
2023-05-22 16:40:54 +0100

    Updated `AdminPlatform (URLS)`

--- apps/admin_platform/urls.py
+++ apps/admin_platform/urls.py
@@ -6,5 +6,9 @@ urlpatterns = [
     path('adminPage', views.adminPage_view, name='adminPage'),
     path('gerirCargosUsers', views.adminGerirCargosUsers_view, name='gerirCargosUsers'),
     path('gerirCargosTeams', views.adminGerirCargosTeams_view, name='gerirCargosTeams'),
-    path('gerirEquipaProcesso', views.adminGerirEquipaProcesso_view, name='gerirEquipaProcesso')
+    path('gerirEquipaProcesso', views.adminGerirEquipaProcesso_view, name='gerirEquipaProcesso'),
+    path('criarTeams', views.adminCriarTeams_view, name='criarTeams'),
+    path('criarTeams/<int:team_id>/', views.adminTeams_delete_view, name='criarTeam_delete'),
+    path('criarSkills/', views.adminCriarSkills_view, name='criarSkills'),
+    path('criarSkills/<int:skill_id>/', views.adminSkills_delete_view, name='criarSkills_delete'),
 ]
