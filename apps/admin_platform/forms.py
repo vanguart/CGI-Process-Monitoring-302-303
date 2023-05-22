c3a7b9bc42165fe06f4e8f84f37ac2d46cf43b36
ricardocleto22006526 <a22006526@alunos.ulht.pt>
2023-05-22 16:40:32 +0100

    Created `Team & Skill (FORM)`

---
+++ apps/admin_platform/forms.py
@@ -0,0 +1,26 @@
+from django.forms import ModelForm
+from django import forms
+
+from main.models import Team, Skill, UserProfile
+from django.contrib.auth.models import Group, User
+
+class criarTeamForm(ModelForm):
+    class Meta:
+        model = Team
+        fields = '__all__'
+
+    #name = forms.CharField(max_length=256)
+    #description = forms.CharField(max_length=100)
+    #idPermissions = forms.ModelMultipleChoiceField(queryset=Group.objects.all(), widget=forms.CheckboxSelectMultiple(), to_field_name='name')
+    #idTeamLider = forms.ModelChoiceField(queryset=UserProfile.objects.all())
+    #idSkils = forms.ModelMultipleChoiceField(queryset=Skill.objects.all(), widget=forms.CheckboxSelectMultiple(), to_field_name='nameSkill')
+
+
+class criarSkillForm(ModelForm):
+    class Meta:
+        model = Skill
+        fields = '__all__'
+
+        labels = {
+            'nameSkill': 'Nome da Skill',
+        }
