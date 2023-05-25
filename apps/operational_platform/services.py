f452288fa3b8cd1704dcabb59f4bc4fa8ce84973
Alexandre-Costa-22007578 <a22007578@alunos.ulht.pt>
2023-05-25 18:52:51 +0100

    add processConfigurationName to logs

--- apps/operational_platform/services.py
+++ apps/operational_platform/services.py
@@ -104,7 +104,7 @@ def createHumanLogs(task, taskData, request):
     timeStampTime = date.split(".")[0] + "." + miliseconds
 
     with open(log.ficheiro.path, 'a') as file:
-       file.writelines(timeStampTime + "\t" + "Process Monitor" + "\t" + "Correction" + "\t" +"The user with id " + str(request.user.id) + "(" + request.user.username + ") completed the correction of the taskData with id = "+ str(taskData.id) + "\t" + str(task.idProcess) + "\n")
+       file.writelines(timeStampTime + "\t" + "Process Monitor" + "\t" + "Correction" + "\t" +"The user with id " + str(request.user.id) + "(" + request.user.username + ") completed the correction of the taskData with id = "+ str(taskData.id) + "\t" + str(task.idProcess.id) + "\t" + task.idProcess.idConfiguration.name + "\n")
 
 
 def excel_to_pdf(input_file, output_file):
