9ad22a71baa40f0bd94b44d0298880c51b81ca8f
Alexandre-Costa-22007578 <a22007578@alunos.ulht.pt>
2023-06-23 13:45:31 +0100

    new folder ID in logs.services.py

--- apps/logs/services.py
+++ apps/logs/services.py
@@ -53,7 +53,7 @@ def connectionToApi():
 
     header['X-UIPATH-TenantName'] = creds['tenancyName']
 
-    header['X-UIPATH-OrganizationUnitId'] = '4497959'
+    header['X-UIPATH-OrganizationUnitId'] = '4500630'
 
     getLogs(creds, header)
 
