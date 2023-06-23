fd8fbcde8d01430fe77e15b09694524c8e26a3de
ricardocleto22006526 <a22006526@alunos.ulht.pt>
2023-06-23 14:38:43 +0100

    New Code Orchestrator

--- apps/logs/services.py
+++ apps/logs/services.py
@@ -53,7 +53,7 @@ def connectionToApi():
 
     header['X-UIPATH-TenantName'] = creds['tenancyName']
 
-    header['X-UIPATH-OrganizationUnitId'] = '4500630'
+    header['X-UIPATH-OrganizationUnitId'] = '4500737'
 
     getLogs(creds, header)
 
