93dd5126b7fc4e6f806ede726327dce384fe41f6
Joao-Eleuterio <a22007237@alunos.ulht.pt>
2023-06-29 21:58:38 +0100

    change email

--- apps/process_listing/templatetags/process_listing_tags.py
+++ apps/process_listing/templatetags/process_listing_tags.py
@@ -6,5 +6,4 @@ register = template.Library()
 def read_log_file(file_path):
     with open(file_path, 'r') as file:
         log_string = file.read()
-        print(log_string)
     return log_string
